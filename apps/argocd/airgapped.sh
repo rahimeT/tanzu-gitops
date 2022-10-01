bf60eb2881ed8e150253973df36003340d2956aa
Gorkem Ozlu <45913400+gorkemozlu@users.noreply.github.com>
2022-10-01 09:12:13 -0700

    added argocd

---
+++ apps/argocd/airgapped.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export harbor="harbor.example.com"
+
+if test -f install.yaml; then
+    echo "install.yaml exists."
+else 
+    wget "https://raw.githubusercontent.com/argoproj/argo-cd/master/manifests/install.yaml"
+fi
+
+for IMAGE in $(cat install.yaml|grep "image:"|sort -u|sed 's/ //g')
+do
+    IMG=${IMAGE#image:}
+    NEW_IMG=$harbor/argo/$IMG
+    echo "burasi "$IMG " " $NEW_IMG
+    docker pull $IMG
+    docker tag $IMG $harbor/argo/$IMG
+    docker push $harbor/argo/$IMG
+    sed -i -e "s~$IMG~$NEW_IMG~g" ./install.yaml
+done
+
+kubectl create ns argo-cd
+kubectl apply -f install.yaml -n argo-cd
+kubectl -n argocd patch svc argocd-server  -p '{"spec": {"type": "LoadBalancer"}}'
+ARGO_PASS=$(kubectl -n argo-cd get secrets argocd-initial-admin-secret -o jsonpath='{.data.password}'|base64 -d)
+ARGO_IP=$(kubectl -n argo-cd get svc argocd-server -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+
+echo " "
+echo "ARGO IP: " "https://"$ARGO_IP
+echo "ARGO Admin: admin"
+echo "ARGO PASS: " $ARGO_PASS
