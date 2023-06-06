6805a0810236759c5599b35003403148b82ebd3d
Gorkem Ozlu <spammailatmayin@gmail.com>
2023-06-06 18:12:47 +0300

    update values-template.yaml

--- tmc-sm/01-setup.sh
+++ tmc-sm/01-setup.sh
@@ -5,12 +5,12 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-export wcp_ip=$(yq eval '.wcp.ip' ./values.yaml)
-export wcp_user=$(yq eval '.wcp.ip' ./values.yaml)
-export wcp_pass=$(yq eval '.wcp.ip' ./values.yaml)
-export namespace=$(yq eval '.shared_cluster.namespace' ./values.yaml)
+export wcp_ip=$(yq eval '.wcp.ip' ./values-template.yaml)
+export wcp_user=$(yq eval '.wcp.ip' ./values-template.yaml)
+export wcp_pass=$(yq eval '.wcp.ip' ./values-template.yaml)
+export namespace=$(yq eval '.shared_cluster.namespace' ./values-template.yaml)
 export tmc_cluster='shared'
-export ldap_auth=$(yq eval '.auth.ldap.enabled' ./values.yaml)
+export ldap_auth=$(yq eval '.auth.ldap.enabled' ./values-template.yaml)
 
 cp ca.crt /etc/ssl/certs/
 
