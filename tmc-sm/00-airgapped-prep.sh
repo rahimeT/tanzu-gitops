8160a72467317c160207c96fba842aa43fc69f4e
Gorkem Ozlu <spammailatmayin@gmail.com>
2023-06-06 18:37:27 +0300

    update order

--- tmc-sm/00-airgapped-prep.sh
+++ tmc-sm/00-airgapped-prep.sh
@@ -26,9 +26,9 @@ elif [ "$1" = "import-packages" ]; then
     echo import-packages
     cp ca.crt /etc/ssl/certs/
     tar -xvf airgapped-files/bundle*.tar
-    ./tmc-local push-images harbor --project harbor.$TLD_DOMAIN/tmc --username admin --password VMware1! --concurrency 10
     export IMGPKG_REGISTRY_USERNAME=admin && export IMGPKG_REGISTRY_PASSWORD='VMware1!' &&  export IMGPKG_REGISTRY_HOSTNAME=harbor.$TLD_DOMAIN
-    curl -u "${IMGPKG_REGISTRY_USERNAME}:${IMGPKG_REGISTRY_PASSWORD}" -X POST -H "content-type: application/json" "https://harbor.${$TLD_DOMAIN}/api/v2.0/projects" -d "{\"project_name\": \"tmc\", \"public\": true, \"storage_limit\": -1 }" -k
+    curl -u "${IMGPKG_REGISTRY_USERNAME}:${IMGPKG_REGISTRY_PASSWORD}" -X POST -H "content-type: application/json" "https://harbor.$TLD_DOMAIN/api/v2.0/projects" -d "{\"project_name\": \"tmc\", \"public\": true, \"storage_limit\": -1 }" -k
+    ./tmc-local push-images harbor --project harbor.$TLD_DOMAIN/tmc --username admin --password VMware1! --concurrency 10
     imgpkg copy --tar airgapped-files/$std_repo.tar --to-repo harbor.$TLD_DOMAIN/tmc/498533941640.dkr.ecr.us-west-2.amazonaws.com/packages/standard/repo --include-non-distributable-layers --registry-ca-cert-path $REGISTRY_CA_PATH
     imgpkg copy --tar airgapped-files/images/kapp-controller-v030.tar --to-repo harbor.$TLD_DOMAIN/tmc/kapp-controller:v0.30.0_vmware.1 --include-non-distributable-layers --registry-ca-cert-path $REGISTRY_CA_PATH
     imgpkg copy --tar airgapped-files/images/busybox.tar --to-repo harbor.$TLD_DOMAIN/tmc/busybox:latest --include-non-distributable-layers --registry-ca-cert-path $REGISTRY_CA_PATH
