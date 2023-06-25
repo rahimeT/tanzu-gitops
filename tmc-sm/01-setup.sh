571b977188634e864dbf1e1b09f53bb24f18b6b4
Gorkem Ozlu <45913400+gorkemozlu@users.noreply.github.com>
2023-06-25 09:35:31 +0300

    enhance readability

--- tmc-sm/01-setup.sh
+++ tmc-sm/01-setup.sh
@@ -4,7 +4,7 @@ if [ $# -ne 1 ]; then
     echo "Usage: $0 vsphere-7|vsphere-8"
     exit 1
 fi
-
+echo "#################################################################################Checking Certificate files#"
 if [ -f ca.crt ] && [ -f ca-no-pass.key ]; then
     echo "required files exist, continuing."
 else
@@ -21,7 +21,7 @@ else
         exit 1
     fi
 fi
-
+echo "###################################################################################Checking vCenter version#"
 export GOVC_URL=$(yq eval '.vcenter.fqdn' ./templates/values-template.yaml)
 export GOVC_INSECURE=1
 
@@ -42,7 +42,7 @@ else
     echo "wrong vcenter build/version. Exiting"
     exit 1
 fi
-
+echo "#######################################################################################Setting-up variables#"
 export wcp_ip=$(yq eval '.wcp.ip' ./templates/values-template.yaml)
 export wcp_user=$(yq eval '.wcp.user' ./templates/values-template.yaml)
 export wcp_pass=$(yq eval '.wcp.password' ./templates/values-template.yaml)
@@ -51,7 +51,7 @@ export tmc_cluster='shared'
 export ldap_auth=$(yq eval '.auth.ldap.enabled' ./templates/values-template.yaml)
 export tmc_dns=$(yq eval '.tld_domain' ./templates/values-template.yaml)
 export hostnames=($tmc_dns *.$tmc_dns)
-
+echo "#####################################################################################Checking DNS A Records#"
 for hostname in "${hostnames[@]}"; do
     output=$(dig +short "$hostname")
     if [[ -n "$output" ]]; then
@@ -65,52 +65,64 @@ done
 cp ca.crt /etc/ssl/certs/
 
 if [ "$1" = "vsphere-7" ]; then
+    echo "####################################################################################Logging into Supervisor#"
     echo vsphere-7
     export KUBECTL_VSPHERE_PASSWORD=$wcp_pass
     kubectl vsphere login --server=$wcp_ip --vsphere-username $wcp_user --insecure-skip-tls-verify
     kubectx $wcp_ip
+    echo "##############################################################Patching TkgServiceConfiguration with CA Cert#"
     export CA_CERT=$(cat ./ca.crt|base64 -w0)
     kubectl patch TkgServiceConfiguration tkg-service-configuration --type merge -p '{"spec":{"trust":{"additionalTrustedCAs":[{"name":"root-ca-tmc","data":"'$(echo -n "$CA_CERT")'"}]}}}'
+    echo "####################################################################################Creating Shared Cluster#"
     ytt -f templates/values-template.yaml -f templates/vsphere-7/shared-cluster.yaml | kubectl apply -f -
     while [[ $(kubectl get tkc shared -o=jsonpath='{.status.conditions[?(@.type=="Ready")].status}' -n $namespace) != "True" ]]; do
-        echo "waiting for cluster to be ready"
+        echo "Waiting for cluster to be ready"
         sleep 30
     done
+    echo "################################################################################Logging into shared cluster#"
     kubectl vsphere login --server=$wcp_ip --tanzu-kubernetes-cluster-name shared --tanzu-kubernetes-cluster-namespace $namespace --vsphere-username $wcp_user --insecure-skip-tls-verify
+    echo "#################################################################Changing kubectl context to shared cluster#"
     kubectx $tmc_cluster
+    echo "###############################################################################################creating psp#"
     kubectl create clusterrolebinding default-tkg-admin-privileged-binding --clusterrole=psp:vmware-system-privileged --group=system:authenticated
+    echo "##################################################################################Deploying kapp-controller#"
     ytt -f templates/common/kapp-controller.yaml -f templates/values-template.yaml | kubectl apply -f -
     while [[ $(kubectl get deployment kapp-controller -n kapp-controller -o=jsonpath='{.status.conditions[?(@.type=="Available")].status}') != "True" ]]; do
-        echo "waiting for kapp-controller to be ready"
+        echo "Waiting for kapp-controller to be ready"
         sleep 10
     done
 elif [ "$1" = "vsphere-8" ]; then
+    echo "####################################################################################Logging into Supervisor#"
     echo vsphere-8
     export KUBECTL_VSPHERE_PASSWORD=$wcp_pass
     kubectl vsphere login --server=$wcp_ip --vsphere-username $wcp_user --insecure-skip-tls-verify
     kubectx $wcp_ip
+    echo "####################################################################################Creating Shared Cluster#"
     ytt -f templates/values-template.yaml -f templates/vsphere-8/shared-cluster.yaml | kubectl apply -f -
     while [[ $(kubectl get cluster shared -o=jsonpath='{.status.conditions[?(@.type=="Ready")].status}' -n $namespace) != "True" ]]; do
-        echo "waiting for cluster to be ready"
+        echo "Waiting for cluster to be ready"
         sleep 30
     done
+    echo "################################################################################Logging into shared cluster#"
     kubectl vsphere login --server=$wcp_ip --tanzu-kubernetes-cluster-name shared --tanzu-kubernetes-cluster-namespace $namespace --vsphere-username $wcp_user --insecure-skip-tls-verify
     kubectx $tmc_cluster
     export node_names=$(kubectl get nodes -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
     while [[ -z "$node_names" ]]; do
         node_names=$(kubectl get nodes -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
         if [[ -z "$node_names" ]]; then
-            echo "waiting for cluster to be ready"
+            echo "Waiting for cluster to be ready"
             sleep 30
         fi
     done
+    echo "###############################################################################################creating psp#"
     kubectl create clusterrolebinding default-tkg-admin-privileged-binding --clusterrole=psp:vmware-system-privileged --group=system:authenticated
 fi
 
 kubectx $tmc_cluster
+echo "######################################################################Deploying Tanzu Standard Package Repo#"
 ytt -f templates/common/std-repo.yaml -f templates/values-template.yaml | kubectl apply -f -
 while [[ $(kubectl get pkgr tanzu-std-repo -n packages -o=jsonpath='{.status.conditions[?(@.type=="ReconcileSucceeded")].status}') != "True" ]]; do
-    echo "waiting for std-repo to be ready"
+    echo "Waiting for std-repo to be ready: " $(kubectl get pkgr tanzu-std-repo -n packages -o=jsonpath='{.status.conditions[0].type}')
     sleep 10
     if [[ $(kubectl get pkgr tanzu-std-repo -n packages -o=jsonpath='{.status.conditions[?(@.type=="ReconcileSucceeded")].status}') == "ReconcileFailed" ]]; then
         kubectl get pkgr tanzu-std-repo -n packages -o=jsonpath='{.status.usefulErrorMessage}'
@@ -119,36 +131,42 @@ while [[ $(kubectl get pkgr tanzu-std-repo -n packages -o=jsonpath='{.status.con
         ytt -f templates/common/std-repo.yaml -f templates/values-template.yaml | kubectl apply -f -
     fi
 done
+echo "#############################################################################Deploying Cert-Manager Package#"
 ytt -f templates/common/cert-manager.yaml -f templates/values-template.yaml | kubectl apply -f -
 while [[ $(kubectl get pkgi cert-manager -n packages -o=jsonpath='{.status.conditions[?(@.type=="ReconcileSucceeded")].status}') != "True" ]]; do
-    echo "waiting for cert-manager to be ready"
+    echo "Waiting for cert-manager to be ready: " $(kubectl get pkgi cert-manager -n packages -o=jsonpath='{.status.conditions[0].type}')
     sleep 10
 done
+echo "#####################################################################Creating ClusterIssuer on Cert-Manager#"
 kubectl create secret tls local-ca --key ca-no-pass.key --cert ca.crt -n cert-manager
 kubectl apply -f templates/common/local-issuer.yaml
 ytt -f templates/values-template.yaml -f templates/common/tmc-values-template.yaml > values.yaml
+echo "##############################################################################Deploying TMC-SM Package Repo#"
 ytt -f templates/values-template.yaml -f templates/common/tmc-repo.yaml | kubectl apply -f -
 while [[ $(kubectl get pkgr tanzu-mission-control-packages -n tmc-local -o=jsonpath='{.status.conditions[?(@.type=="ReconcileSucceeded")].status}') != "True" ]]; do
-    echo "waiting for tmc-repo to be ready"
+    echo "Waiting for tmc-repo to be ready: " $(kubectl get pkgr tanzu-mission-control-packages -n tmc-local -o=jsonpath='{.status.conditions[0].type}')
     sleep 10
 done
 
 export valuesContent=$(cat values.yaml)
+echo "###################################################################################Deploying TMC-SM Package#"
 ytt -f templates/values-template.yaml --data-value valuesContent="$valuesContent" -f templates/common/tmc-install.yaml | kubectl apply -f -
 while [[ $(kubectl get pkgi tanzu-mission-control -n tmc-local -o=jsonpath='{.status.conditions[?(@.type=="ReconcileSucceeded")].status}') != "True" ]]; do
-    echo "waiting for tanzu-mission-control to be ready"
+    echo "Waiting for tanzu-mission-control to be ready: " $(kubectl get pkgi tanzu-mission-control -n tmc-local -o=jsonpath='{.status.conditions[0].type}')
     sleep 10
 done
 if [ "$ldap_auth" = "true" ]; then
-   ytt -f templates/values-template.yaml -f templates/common/openldap.yaml | kubectl apply -f -
+    echo "#########################################################################################Deploying OpenLDAP#"
+    ytt -f templates/values-template.yaml -f templates/common/openldap.yaml | kubectl apply -f -
     kubectl apply -f templates/common/ldap-overlay.yaml
     while [[ $(kubectl get deployment openldap -n openldap -o=jsonpath='{.status.conditions[?(@.type=="Available")].status}') != "True" ]]; do
-        echo "waiting for openldap to be ready"
+        echo "Waiting for openldap to be ready"
         export openldapCaCert=$(kubectl get secret ldap -n openldap -o json | jq -r '.data."ca.crt"')
         sleep 10
     done
     sleep 30
     export openldapCaCert=$(kubectl get secret ldap -n openldap -o json | jq -r '.data."ca.crt"')
+    echo "######################################################################################Applying LDAP Overlay#"
     ytt -f  templates/common/ldap-auth.yaml --data-value ldapCa=$openldapCaCert | kubectl apply -f -
     kubectl annotate packageinstalls tanzu-mission-control -n tmc-local ext.packaging.carvel.dev/ytt-paths-from-secret-name.0=tmc-overlay-override
     kubectl patch -n tmc-local --type merge pkgi tanzu-mission-control --patch '{"spec": {"paused": true}}'
@@ -164,14 +182,18 @@ yq e -i ".spec.caCerts = strenv(caCert)" ./templates/common/agentconfig.yaml
 yq e -i ".spec.allowedHostNames = [env(tmcURL)]" ./templates/common/agentconfig.yaml
 yq e -i ".metadata.namespace = strenv(tmcNS)" ./templates/common/agentconfig.yaml
 yq e -i ".metadata.namespace = strenv(tmcNS)" ./templates/common/agentinstall.yaml
-
+echo "#######################################################################################Applying AgentConfig#"
 kubectl apply -f ./templates/common/agentconfig.yaml
 
 kubectx $tmc_cluster
+echo "##################################################################################Finished Deploying TMC-SM#"
 echo "-------------------"
 echo Open TMC-SM via this URL: https://$tmc_dns
 echo "-------------------"
-echo "if on vSphere 8, run below command on supervisor level before creating each new workload cluster "
-echo " "
-echo "ytt -f templates/values-template.yaml -f templates/vsphere-8/cluster-config.yaml | kubectl apply -f -"
-echo " "
+
+if [[ "$vCenter_version" == "8.0.1" ]]; then
+    echo "Run below command on supervisor level before creating each new workload cluster "
+    echo " "
+    echo "ytt -f templates/values-template.yaml -f templates/vsphere-8/cluster-config.yaml | kubectl apply -f -"
+    echo " "
+fi
